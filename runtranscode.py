c3fcdd110fcab9cd2bb851827ffa0e0bb0c6479e
Aaron Davis <mlad@google.com>
2024-06-28 14:46:03 -0600

    even similer

--- runtranscode.py
+++ runtranscode.py
@@ -4,26 +4,23 @@ import os
 import subprocess
 import time
 
-
 #time to wait before next split
-pause_time = 300 #5 min
+pause_time = 360 #6 min
 # Set the starting file
-start_file = "split_aah"  # Replace with the desired starting file
+start_file = "split_aau"  # Replace with the desired starting file
 
-# Main script
-if __name__ == "__main__":
-    # Get a list of files starting with 'filesplit*'
-    files = [f for f in os.listdir(".") if f.startswith("split_*")]
-    files.sort()  # Sort the files alphabetically  
+# Get a list of files starting with 'filesplit*'
+files = [f for f in os.listdir(".") if f.startswith("split_*")]
+files.sort()  # Sort the files alphabetically  
 
-    # Iterate through the files, starting from the specified file
-    for i, file in enumerate(files):
-        if file == start_file:
-            print(f"Starting from file: {file}")
-            break
+# Iterate through the files, starting from the specified file
+for i, file in enumerate(files):
+    if file == start_file:
+        print(f"Starting from file: {file}")
+        break
 
-    for file in files[i:]:
-        print(f"Executing file: {file}")
-        # Execute the file with '/bin/sh'
-        subprocess.run(["/bin/sh", file])
+for file in files[i:]:
+    print(f"Executing file: {file}")
+    # Execute the file with '/bin/sh'
+    subprocess.run(["/bin/sh", file])
 
